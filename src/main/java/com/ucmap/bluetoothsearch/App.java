35f22be687e8cdd2f96c426c70d6919b23ba0515
Justson <cenxiaozhong.qqcom@qq.com>
2016-10-03 22:47:33 +0800

    Just

---
+++ src/main/java/com/ucmap/bluetoothsearch/App.java
@@ -0,0 +1,37 @@
+package com.ucmap.bluetoothsearch;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * 作者: Justson
+ * 时间:2016/10/3 9:13.
+ * 邮箱: cenxiaozhong.qqcom@qq.com
+ * 公司: YGS
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code...
+
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(base);
+    }
+}
---
+++ src/main/java/com/ucmap/bluetoothsearch/PairReceiver.java
@@ -0,0 +1,66 @@
+package com.ucmap.bluetoothsearch;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telecom.Log;
+
+/**
+ * 作者: Justson
+ * 时间:2016/10/2 13:55.
+ * 邮箱: cenxiaozhong.qqcom@qq.com
+ * 公司: YGS
+ */
+
+public class PairReceiver extends BroadcastReceiver {
+
+    private BluetoothDevice mBluetoothDevice;
+
+    private String target;
+
+    private PairCallBack mPairCallBack;
+
+    public PairReceiver(String target, PairCallBack pairCallBack) {
+        this.target = target;
+        this.mPairCallBack = pairCallBack;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        String action = intent.getAction();
+        if (action.equals(BluetoothDevice.ACTION_FOUND)) {
+            mBluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            if (mBluetoothDevice.getName().equals(target)) {
+                try {
+                    ClsUtils.createBond(mBluetoothDevice.getClass(), mBluetoothDevice);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if (action.equals("android.bluetooth.device.action.PAIRING_REQUEST")) {
+            Log.i("Infoss", "action");
+            BluetoothDevice mBluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            if (mBluetoothDevice.getName().equals(target)) {
+                try {
+                    abortBroadcast();//终止有序广播
+                    ClsUtils.setPairingConfirmation(mBluetoothDevice.getClass(), mBluetoothDevice, true);
+                    if (mPairCallBack != null)
+                        mPairCallBack.onPairFinish();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.i("Infoss", "不是目标设备:" + mBluetoothDevice.getName());
+            }
+
+        }
+
+    }
+
+    interface PairCallBack {
+        void onPairFinish();
+    }
+}
