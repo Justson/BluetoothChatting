1ed352fdd5912d1c689dafef8aacf26758860860
Justson <xiaozhongcen@gmail.com>
2021-04-03 23:57:53 +0800

    Update BluetoothServer.java

--- src/main/java/com/ucmap/bluetoothsearch/main/BluetoothServer.java
+++ src/main/java/com/ucmap/bluetoothsearch/main/BluetoothServer.java
@@ -20,15 +20,7 @@ import java.util.concurrent.TimeUnit;
 
 import static java.lang.Integer.MAX_VALUE;
 
-/**
- * <b>@项目名：</b> uPatch<br>
- * <b>@包名：</b><br>
- * <b>@创建者：</b> cxz<br>
- * <b>@创建时间：</b> &{DATE}<br>
- * <b>@公司：</b> 优高胜公司<br>
- * <b>@邮箱：</b> cenxiaozhong.qqcom@qq.com<br>
- * <b>@描述</b><br>
- */
+
 
 public class BluetoothServer {
 
